984d6cb6b12746d396bd32da3a6a081913347a64
nehank1521 <nkneha1521@gmail.com>
2017-07-06 01:10:26 +0530

    Add files via upload

---
+++ Application.feature
@@ -0,0 +1,17 @@
+
+@tag
+Feature: Take snapshots up to 5Fivepages
+	I want to use this template for my feature file
+@tag1
+Scenario: Title of your scenario
+Given Browse the stackoverflow url
+	And  provide valid credentials
+When Login page should open
+	And Browse the url
+	And Getting text
+	And taking snapshot 
+	And Click on next
+	And Taking snap shots up to 5 pages
+Then Logout
+	And Close the browser
+
---
+++ cucumber.java
@@ -0,0 +1,59 @@
+package StepDefinition;
+
+public class cucumber 
+{
+
+	@Given("^Browse the stackoverflow url$")
+	public void browse_the_stackoverflow_url() throws Throwable {
+	  System.out.println("Hello");
+	}
+
+	@Given("^provide valid credentials$")
+	public void provide_valid_credentials() throws Throwable {
+	   
+	}
+
+	@When("^Login page should open$")
+	public void login_page_should_open() throws Throwable {
+	   
+	}
+
+	@When("^Browse the url$")
+	public void browse_the_url() throws Throwable {
+	  
+	}
+
+	@When("^Getting text$")
+	public void getting_text() throws Throwable {
+	 
+	}
+
+	@When("^taking snapshot$")
+	public void taking_snapshot() throws Throwable {
+	  ;
+	}
+
+	@When("^Click on next$")
+	public void click_on_next() throws Throwable {
+	   
+	}
+
+	@When("^Taking snap shots up to (\\d+) pages$")
+	public void taking_snap_shots_up_to_pages(int arg1) throws Throwable {
+	  
+	}
+
+	@Then("^Logout$")
+	public void logout() throws Throwable {
+	  
+	}
+
+	@Then("^Close the browser$")
+	public void close_the_browser() throws Throwable {
+	
+	}
+
+
+	
+
+}
